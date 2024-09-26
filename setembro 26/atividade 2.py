1fd72fb194439ef75c3f74421d2fc816bdf4ddda
Eliasdl007 <eliasjose0798@gmail.com>
2024-09-26 20:00:49 +0000

    SEMTEMBRO

---
+++ setembro 26/atividade 2.py
@@ -0,0 +1,13 @@
+import os
+from dataclasses import dataclass
+
+os.system("cls || clear")
+
+class Client :
+    nome:str
+    sobrenome:str
+    idade:int
+    peso:float
+    altura:float
+
+QUANTIDADE_ALUNO = 4
